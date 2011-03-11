ca4f827b5bf58dc2941a5269e3b1ba59b8e015e0
seke.nenad <seke.nenad@4f10cb1d-2049-2383-0a48-ac47ac52ff3b>
2011-03-11 15:01:33 +0000

    poceo sam da realizujem dodavanje Genre-a u bazu.

---
+++ filmania/src/main/java/org/seke/filmania/dao/GenreDAO.java
@@ -0,0 +1,14 @@
+package org.seke.filmania.dao;
+
+import org.seke.filmania.domain.Genre;
+
+/**
+ * Interface for Genre DAO
+ * 
+ * @author nenad.seke
+ * 
+ */
+public interface GenreDAO {
+
+	public void create(Genre genre);
+}
