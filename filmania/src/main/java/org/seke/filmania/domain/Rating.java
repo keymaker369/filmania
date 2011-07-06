da43a5a6ee07a5029da501e510b34aa91f1ff532
seke.nenad <seke.nenad@4f10cb1d-2049-2383-0a48-ac47ac52ff3b>
2011-07-06 08:24:45 +0000

    Altered Rating domain object.

--- filmania/src/main/java/org/seke/filmania/domain/Rating.java
+++ filmania/src/main/java/org/seke/filmania/domain/Rating.java
@@ -1,96 +1,102 @@
 package org.seke.filmania.domain;
 
 import java.io.Serializable;
+import java.util.Date;
 
+import javax.persistence.AttributeOverride;
+import javax.persistence.AttributeOverrides;
+import javax.persistence.Column;
+import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.FetchType;
 import javax.persistence.IdClass;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
 @Entity
-@Table(name = "RATING")
-public class Rating {
+@Table(name = "RATING", catalog = "filmania")
+public class Rating implements Serializable {
 
-	@Id
+	@EmbeddedId
+	@AttributeOverrides({ 
+		@AttributeOverride(name = "id", column = @Column(name = "ID", nullable = false)), 
+		@AttributeOverride(name = "userId", column = @Column(name = "USER_ID", nullable = false)),
+		@AttributeOverride(name = "movieId", column = @Column(name = "MOVIE_ID", nullable = false)) })
 	private RatingId ratingId;
 
-	//
-	// @ManyToOne
-	// private User user;
-	//
-	// @ManyToOne
-	// private Movie movie;
-
-	// private int value;
-	//
-	 public RatingId getRatingId() {
-	 return ratingId;
-	 }
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "USER_ID", nullable = false, insertable = false, updatable = false)
+	private User user;
 	
-	 public void setRatingId(RatingId ratingId) {
-	 this.ratingId = ratingId;
-	 }
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "MOVIE_ID", nullable = false, insertable = false, updatable = false)
+	private Movie movie;
+	
+	@Column(name = "MARK")
+	private int mark;
 
-	// public User getUser() {
-	// return user;
-	// }
-	//
-	// public void setUser(User user) {
-	// this.user = user;
-	// }
-	//
-	// public Movie getMovie() {
-	// return movie;
-	// }
-	//
-	// public void setMovie(Movie movie) {
-	// this.movie = movie;
-	// }
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "INPUTDATE", length = 19)
+	private Date inputDate;
 
-	// public int getValue() {
-	// return value;
-	// }
-	//
-	// public void setValue(int value) {
-	// this.value = value;
-	// }
-
-	@IdClass(Rating.class)
-	public static class RatingId implements Serializable {
-		/**
-		 * 
-		 */
-		private static final long serialVersionUID = 4078766891792538922L;
-		@ManyToOne
-		private User user;
-		@ManyToOne
-		private Movie movie;
-		private int value;
-
-		public User getUser() {
-			return user;
-		}
-
-		public void setUser(User user) {
-			this.user = user;
-		}
-
-		public Movie getMovie() {
-			return movie;
-		}
-
-		public void setMovie(Movie movie) {
-			this.movie = movie;
-		}
-
-		public int getValue() {
-			return value;
-		}
-
-		public void setValue(int value) {
-			this.value = value;
-		}
-
+	public Rating() {
 	}
+
+	public Rating(RatingId ratingId, User user, Movie movie) {
+		this.ratingId = ratingId;
+		this.user = user;
+		this.movie = movie;
+	}
+
+	public Rating(RatingId ratingId, User user, Movie movie, int mark, Date inputDate) {
+		this.ratingId = ratingId;
+		this.user = user;
+		this.movie = movie;
+		this.mark = mark;
+		this.inputDate = inputDate;
+	}
+
+	public RatingId getRatingId() {
+		return ratingId;
+	}
+
+	public void setRatingId(RatingId ratingId) {
+		this.ratingId = ratingId;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public Movie getMovie() {
+		return movie;
+	}
+
+	public void setMovie(Movie movie) {
+		this.movie = movie;
+	}
+
+	public int getMark() {
+		return mark;
+	}
+
+	public void setMark(int mark) {
+		this.mark = mark;
+	}
+
+	public Date getInputDate() {
+		return inputDate;
+	}
+
+	public void setInputDate(Date inputDate) {
+		this.inputDate = inputDate;
+	}
+
 }
